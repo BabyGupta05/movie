d9b4c41251216a2e8d1a1361f98311c5756e9057
BabyGupta05 <codecode355@gmail.com>
2024-07-18 16:59:12 +0530

    api url update

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import axios from "axios";
 
 export const Search = () => {
-  const API_URL = "http://www.omdbapi.com/?apikey=1696d76b";
+  const API_URL = "https://www.omdbapi.com/?apikey=1696d76b";
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
